aa5be47ae9c42ce60d7eed0db3b09829c55f39b3
SANJU <89297664+LolanBot18@users.noreply.github.com>
2022-01-16 11:35:13 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -45,3 +45,17 @@ ABOUT = """
 
 ➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
 """
+
+FUN = """
+➥ Name : Auto Filter With IMDB
+    
+➥ Credits : Subin
+
+➥ Editing : [Muhammed RK](t.me/Mrk_YT)
+
+➥ Language : Python3
+
+➥ Library : Pyrogram Asyncio 1.13.0
+
+➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+"""
