a32d61a87f5ce50f1272c371e0ded5c62406cce6
SANJU <89297664+LolanBot18@users.noreply.github.com>
2022-01-16 09:53:44 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -110,10 +110,9 @@ async def start(bot, message):
             )
         )
     else:
-        await message.reply_text(
-            START_MSG.format(message.from_user.first_name),
-            parse_mode="Markdown",
-            disable_web_page_preview=True,
+        await cmd.reply_photo(
+            photo=f"{random.choice(PICS)}",
+            caption=START_MSG,
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
