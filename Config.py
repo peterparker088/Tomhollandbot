f2091e0310ece5a0410efa76fac02870f23ce6b9
SANJU <89297664+LolanBot18@users.noreply.github.com>
2022-01-18 11:10:11 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -44,10 +44,10 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 # Messages
 default_start_msg = """
 **Hi, I'm Auto Filter V3**
-
 Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
+START_IMG = environ.get("START_IMG", "")
 
 FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
 OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
