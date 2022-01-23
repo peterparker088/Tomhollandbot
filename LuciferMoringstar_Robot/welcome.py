6a4b368d765f518b156fd8780ac68730e79ffd9e
SANJU <89297664+LolanBot18@users.noreply.github.com>
2022-01-23 18:53:39 +0530

    Update welcome.py

--- LuciferMoringstar_Robot/welcome.py
+++ LuciferMoringstar_Robot/welcome.py
@@ -1,17 +1,25 @@
-import os
+# Code By @PeterParkerspide
+
+import asyncio
 from pyrogram import Client, filters
-from pyrogram.types import Message, User
-
-
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.new_chat_members)
-async def welcome(bot,message):
-	chatid= message.chat.id
-	await bot.send_message(text=f"Hey , {u.mention}, Welcome to {message.chat.title}",chat_id=chatid)
+async def join(client, message):
+    new_member = message.new_chat_members[0]
+    msg = await message.reply_video("https://telegra.ph/file/b24dc362daae24b98fb7c.mp4",
+                caption = f"""
+**🙌Hey {new_member.first_name},**
+**[{message.chat.title}] || Movie Requesting Group** ലേക്ക് സ്വാഗതം
+നിങ്ങൾക്ക് വേണ്ട സിനിമയുടെ പേരും വർഷവും കൃത്യമായി **[Google](https://google.com/) ൽ നിന്നും നോക്കി അയക്കുക Movie എന്ന വാക്ക് അയക്കേണ്ട 😄. നമ്മുടെ ഗ്രൂപ്പ് പരമാവതി ♻️Share And Support♻️ ചെയ്യുക.☺️**
+""", parse_mode="md",
+     reply_markup = InlineKeyboardMarkup(
+      [[
+        InlineKeyboardButton("🔰 Canneil 🔰", url="https://t.me/flimsupdate")
+      ],
+      [
+        InlineKeyboardButton("♻️ Share Group ♻️", url="https://t.me/share/url?url="https://t.me/tomhollandmovies")
+     ]]))
 
-@Client.on_message(filters.left_chat_member)
-async def goodbye(bot,message):
-	chatid= message.chat.id
-	await bot.send_message(text=f"Bye ,  {u.mention} , Have a Nice Day",chat_id=chatid)
-	
-
+    await asyncio.sleep(50)
+    await msg.delete()
