4f81c56937fda6313a34c1e7ccd192d3144f11da
SANJU <89297664+LolanBot18@users.noreply.github.com>
2022-01-30 19:04:24 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -324,16 +324,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 InlineKeyboardButton('⚠️𝖧𝖾𝗅𝗉', callback_data='help'),
                 InlineKeyboardButton('𝖢𝗅𝗈𝗌𝖾🗑️', callback_data='close')
                 ]]
-        await query.message.edit_text(
-            text="▣▢▢"
-        )
-        await query.message.edit_text(
-            text="▣▣▢"
-        )
-        await query.message.edit_text(
-            text="▣▣▣"
-        )
-        await query.message.edit(text=f"{ABOUT}".format(TUTORIAL), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+            await query.message.edit(text=f"{ABOUT}".format(TUTORIAL), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
         elif query.data == "rules":
             buttons = [
